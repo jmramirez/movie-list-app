eb239599506d4ca9585bea111bf5a72f446c0018
Jose Ramirez <josemramirez81@outlook.com>
2021-11-10 00:26:27 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace movie_app_list.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
